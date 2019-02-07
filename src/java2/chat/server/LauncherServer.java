8c8429b0e98b762ca67e3007d888a016174ea5bc
darkpradoJava <darkpradoJava@yandex.ru>
2019-02-07 10:33:21 +0300

    Разобрался с кодом

---
+++ src/java2/chat/server/LauncherServer.java
@@ -0,0 +1,11 @@
+package java2.chat.server;
+
+/**
+ * @author Stanislav Melekhin
+ * email darkprado@yandex.ru
+ * Created on 07/02/2019
+ */
+
+public class LauncherServer {
+    Server server = new Server();
+}
---
+++ src/java2/chat/server/Server.java
@@ -0,0 +1,55 @@
+package java2.chat.server;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Stanislav Melekhin
+ * email darkprado@yandex.ru
+ * Created on 07/02/2019
+ */
+
+public class Server {
+
+    private final static int PORT = 4004;
+
+    private ServerSocket server = null;
+    private Socket socket = null;
+    private ClientHandler newClient = null;
+    private List<ClientHandler> clients = new ArrayList<>();
+
+    public Server() {
+
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("Сервер запущен...");
+            while (true) {
+                socket = server.accept();
+                newClient = new ClientHandler(socket, this);
+                clients.add(newClient);
+                new Thread(newClient).start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+                server.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void sendMsgToAllClients(String msgText) {
+        clients.forEach(clientHandler -> clientHandler.sendMessage(msgText));
+    }
+
+    public void removeClient(ClientHandler clientHandler) {
+        clients.remove(clientHandler);
+    }
+
+}
